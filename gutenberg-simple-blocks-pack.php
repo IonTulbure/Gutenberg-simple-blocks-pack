5a6a0b5236e1c81b861be05b236ed57211b8dd8a
Johnny <iontulburedev@gmail.com>
2024-02-05 12:15:57 +0200

    register demo block

--- gutenberg-simple-blocks-pack.php
+++ gutenberg-simple-blocks-pack.php
@@ -31,3 +31,15 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Current plugin version.
  */
 define( 'GUTENBERG_SIMPLE_BLOCKS_PACK_VERSION', '1.0.0' );
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function __block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', '__block_init' );
