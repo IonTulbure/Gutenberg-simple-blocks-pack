72132e813e184ad84dda1fb875024aba9779df09
Johnny <iontulburedev@gmail.com>
2024-02-05 11:11:46 +0200

    added plugin header comment

---
+++ gutenberg-simple-blocks-pack.php
@@ -0,0 +1,21 @@
+/**
+ * Gutenberg Simple Blocks Pack
+ *
+ * @package           GutenbergSimpleBlocksPack
+ * @author            Ion Tulbure
+ * @copyright         2024 Ion Tulbure
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Gutenberg Simple Blocks Pack
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       A collection of blocks to extend Gutenberg.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ion Tulbure
+ * Author URI:        https://mustcode.xyz
+ * Text Domain:       gutenberg-simple-blocks-pack
+ * License:           GPL v3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+ */
