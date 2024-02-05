63a58f0fa2ab46045fad714942c98d764f7333a3
Johnny <iontulburedev@gmail.com>
2024-02-05 14:57:41 +0200

    horizontal slider block init

---
+++ build/horizontal-slider/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '90fd3649ab238fa240cb');
---
+++ build/horizontal-slider/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '23289b96a2ff52e6593b');
