19b48e820dd68b22b35703f5d61c6077c5e64444
Johnny <iontulburedev@gmail.com>
2024-02-05 16:19:25 +0200

    added static accordion block

---
+++ build/accordion/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '99a288ff6d54fabd9a60');
---
+++ build/accordion/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd0c4913038c96b6a3d10');
