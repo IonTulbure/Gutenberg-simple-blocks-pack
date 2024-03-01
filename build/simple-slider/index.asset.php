a53773696daa4cb351ac3d7c3d9124abdf046062
Johnny <iontulburedev@gmail.com>
2024-03-01 11:26:38 +0200

    img responsive css for editor screen

--- build/simple-slider/index.asset.php
+++ build/simple-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e7479d5cd4339f7388fd');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1408b0090e3d31ccdf46');
