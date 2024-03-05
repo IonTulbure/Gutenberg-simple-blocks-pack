adc5070135ab41bd7fd3a9cc0a398ef9931361e5
Johnny <iontulburedev@gmail.com>
2024-03-05 12:15:33 +0200

    limit width of slider imgs in editor

--- build/simple-slider/index.asset.php
+++ build/simple-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'b7dbb1f4ed77780aa874');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'f1c15d83b79ccdb5a293');
