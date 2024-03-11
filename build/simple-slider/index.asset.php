a5cab925db2ef98b4b1cf53b28404903f27cb1df
Johnny <iontulburedev@gmail.com>
2024-03-11 12:59:07 +0200

    replaced flex with grid for slider container

--- build/simple-slider/index.asset.php
+++ build/simple-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '3367b779b9b138f33b42');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '837ef514cab107071f9f');
