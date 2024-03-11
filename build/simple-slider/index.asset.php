2bb582e481c86f8ef2f2441c1a9cc9abfde68502
Johnny <iontulburedev@gmail.com>
2024-03-11 13:04:53 +0200

    align center figcaption

--- build/simple-slider/index.asset.php
+++ build/simple-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '3e7b6a46c35e50160359');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '59ae2c24e1bfff254dc3');
