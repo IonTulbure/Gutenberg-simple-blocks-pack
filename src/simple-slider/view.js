18311f77cb2afb1e0373aff35bca88e355f674d5
ion Tulbure <ion.tulbure@feel-it-services.com>
2024-04-14 23:28:13 +0300

    Add scrollbar to the saved html

--- src/simple-slider/view.js
+++ src/simple-slider/view.js
@@ -22,14 +22,17 @@
 
 /* eslint-disable no-console */
 
-import { Swiper } from 'swiper';
-import { Autoplay, Keyboard, Navigation, Pagination } from 'swiper/modules';
+import Swiper from 'swiper';
+import { Autoplay, Keyboard, Navigation, Pagination, Scrollbar } from 'swiper/modules';
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+
+// import styles bundle
+import 'swiper/css/bundle';
 
 // import "./styles.css";
 
@@ -37,6 +40,10 @@ import 'swiper/css/scrollbar';
 
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper', {
+
+        // configure Swiper to use modules
+        modules: [Navigation, Pagination, Autoplay, Keyboard, Scrollbar],
+
         // Optional parameters
         direction: 'horizontal',
         autoplay: {
