f167df966f986aedf33c567f73e9af74e1179b4b
IonTulbure <iontulburedev@gmail.com>
2024-04-24 11:41:01 +0300

    refactor: include view style css for frontend

--- build/simple-slider/index.asset.php
+++ build/simple-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '45eb65f58bb64bcaeb01');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '57cdf1b646d0c1c0992d');
